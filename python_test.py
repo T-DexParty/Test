6d89cb0afcfe4f71fcafe726ac2feffe2724b825
T_DexParty <dexnamkung@gmail.com>
2026-04-07 19:48:09 -0400

    Initialized project

---
+++ python_test.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+print("Hello World")
+x= np.linspace(0, 10, 100)
+plt.plot([0, 1], [0, 1], 'r-')
+plt.show()
+# read .cp file and plot the lines (line type(x,1,y1,x2,y2))
+# take a flat-foldable crease pattern and print the x-ray
